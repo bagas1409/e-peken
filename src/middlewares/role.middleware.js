572e916b45fefeb6b0dee521ad38f2dddfd756b4
bagas1409 <bagaspermana1409@gmail.com>
2026-01-23 09:27:27 +0700

    chore: add debug logging

--- src/middlewares/role.middleware.js
+++ src/middlewares/role.middleware.js
@@ -1,6 +1,7 @@
 export const roleMiddleware = (allowedRoles = []) => {
   return (req, res, next) => {
     if (!allowedRoles.includes(req.user.role)) {
+      console.log(`[AUTH DEBUG] 403 Forbidden. User Role: '${req.user.role}', Allowed: ${JSON.stringify(allowedRoles)}`);
       return res.status(403).json({
         message: "Akses ditolak",
       });
